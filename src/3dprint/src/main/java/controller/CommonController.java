ca283130c407cec588f6db178cc06ea872d06583
alisdame <ldl434020@126.com>
2015-06-11 00:29:14 +0800

    用户上传头像功能完成。

--- src/3dprint/src/main/java/controller/CommonController.java
+++ src/3dprint/src/main/java/controller/CommonController.java
@@ -1,22 +1,14 @@
 package controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
 import java.util.Map;
 
 import javax.annotation.Resource;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
 import service.UserService;
 
